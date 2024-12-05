8ad9109fb1f0b117b991033d9da741c472149a47
Ericdroush <125920690+Ericdroush@users.noreply.github.com>
2024-12-04 19:08:24 -0500

    Updated with Day 1 (parts 1 and 2, and Day 2, part 1.

--- 2024/Code/d2.py
+++ 2024/Code/d2.py
@@ -4,12 +4,7 @@ Day 3
 """
 import numpy as np
 
-with open('d2_input.txt') as f:
-    lines = f.readlines()
-
-safe, unsafe = 0, 0
-for line in lines:
-    report = [int(x.strip()) for x in line.split()]
+def check_report(report):
     slope = np.sign(report[1] - report[0])
     status = True
     for i in range(len(report) - 1):
@@ -20,10 +15,28 @@ for line in lines:
             if abs(report[i + 1] - report[i]) > 3:
                 status = False
                 break
+    return status
 
+
+with open('d2_input.txt') as f:
+    lines = f.readlines()
+
+safe, unsafe, semisafe = 0, 0, 0
+for line in lines:
+    report = [int(x.strip()) for x in line.split()]
+    status = check_report(report)
     if status:
         safe += 1
     else:
         unsafe += 1
+        for i in range(len(report)):
+            report2 = report[:i] + report[i+1:]
+            status = check_report(report2)
+            if status:
+                semisafe += 1
+                unsafe -= 1
+                break
 
-print('Total reports:', len(lines), 'Safe:', safe, 'Unsafe:', unsafe)
+
+print('Total reports:', len(lines), 'Safe:', safe, 'Semisafe:', semisafe, 'Unsafe:', unsafe)
+print('Total safe:', safe + semisafe)
